0e0c2b9965c2fd095a0a4d90afd16e96846fcedf
Gabriel Moreira <lavarinimoreira@gmail.com>
2025-01-29 22:44:39 -0300

    New admin functionalities: read all user, delete user by id

--- routers/admin.py
+++ routers/admin.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status, Path
 from models import Todos as TodosModel
+from models import Users as UserModel
 from database import SessionLocal
 from .auth import get_current_user
 
@@ -31,13 +32,20 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 # READ routers --------------------------------------------------------------
 
-# read_all
+# read_all_todos
 @router.get('/todo', status_code=status.HTTP_200_OK)
-async def read_all(user: user_dependency, db: db_dependency):
+async def read_all_todos(user: user_dependency, db: db_dependency):
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=401, detail='Authentication Failed.')
     return db.query(TodosModel).all()
 
+
+# read_all_users
+@router.get('/users', status_code=status.HTTP_200_OK)
+async def read_all_users(user: user_dependency, db: db_dependency):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed.')
+    return db.query(UserModel).all()
 
 
 
@@ -55,3 +63,19 @@ async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = P
 
     db.query(TodosModel).filter(TodosModel.id == todo_id).delete()
     db.commit()
+
+
+# delete_user
+@router.delete('/user/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user: user_dependency, db: db_dependency, user_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed.')
+    user_model = db.query(UserModel).filter(UserModel.id == user_id).first()
+
+    if user_model is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found.')
+
+    db.query(UserModel).filter(UserModel.id == user_id).delete()
+    db.commit()
+
+
